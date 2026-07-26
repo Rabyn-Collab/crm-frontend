febb0db487fe208878d8536b7d0252eef2df59af
jumper900 <48655154+jumper900@users.noreply.github.com>
2026-07-26 09:32:50 +0545

    Clear git cache

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: 'https://crm-backened.onrender.com',
+  baseURL: 'https://deck-production-35f5.up.railway.app',
   //baseURL: 'http://localhost:5000',
   withCredentials: true,
   headers: {
