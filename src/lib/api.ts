a7692882133df2d421018b8a0a827d5387c86a1e
jumper900 <48655154+jumper900@users.noreply.github.com>
2026-07-26 10:25:11 +0545

    Clear git cache

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const api = axios.create({
   baseURL: 'https://crm-backened.onrender.com',
-  //baseURL: 'http://localhost:5000',
+  // baseURL: 'http://localhost:5000',
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
