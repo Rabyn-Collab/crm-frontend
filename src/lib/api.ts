3aa5e37d4666b7b29e2005464485b25133865f13
Rabyn-Collab <babynshrestha64@gmail.com>
2026-07-26 08:23:34 +0545

    Clear git cache

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: 'https://crm-backened.onrender.com',
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://crm-backened.onrender.com',
+  //baseURL: 'http://localhost:5000',
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
